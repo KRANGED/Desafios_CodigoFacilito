5a1f2295214ae92b14da80506190fbf64469c536
KRANGED <159100127+KRANGED@users.noreply.github.com>
2024-02-05 21:54:03 -0600

    Add files via upload

---
+++ dia1.py
@@ -0,0 +1,8 @@
+
+##Desafío del día 1
+nombre              =   input('Ingresa tu(s) nombre(s): ')
+apellidos           =   input('Ingresa tu(s) apellido(s): ')
+numero_de_telefono  =   input('Ingresa tu numero de telefono: ')
+correo_electronico  =   input('Ingresa tu correo electronico:')
+
+print('Hola' + ' ' + nombre + ' ' + apellidos + ' en breve recibirás un correo a '+ correo_electronico)
